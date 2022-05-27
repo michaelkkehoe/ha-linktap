fb2f4f3e8265a188f09941e51fa3d3d076c7db95
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-26 17:17:20 -0700

    Update __init__.py

--- custom_components/linktap/__init__.py
+++ custom_components/linktap/__init__.py
@@ -1,1 +1,54 @@
+"""The flo integration."""
+import asyncio
+import logging
 
+from linktap import LinkTap
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_KEY, CONF_USERNAME, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .const import CLIENT, DOMAIN
+from .device import FloDeviceDataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up flo from a config entry."""
+    session = async_get_clientsession(hass)
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
+    try:
+        hass.data[DOMAIN][entry.entry_id][CLIENT] = client = LinkTap(
+            entry.data[CONF_USERNAME], entry.data[CONF_API_KEY]
+        )
+    except Exception as err:
+        raise ConfigEntryNotReady from err
+
+    devices = await client.get_all_devices()
+
+    _LOGGER.error("Linktap Devices: %s", devices)
+
+    hass.data[DOMAIN][entry.entry_id]["devices"] = devices = [
+        LinktapDeviceDataUpdateCoordinator(hass, client, location["id"], device["id"])
+        for location in user_info["locations"]
+        for device in location["devices"]
+    ]
+
+    tasks = [device.async_refresh() for device in devices]
+    await asyncio.gather(*tasks)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
