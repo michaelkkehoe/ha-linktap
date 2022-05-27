37f9db1de93cd2342151fe5c3cc794cbdbe46e30
michaelkkehoe <4047274+michaelkkehoe@users.noreply.github.com>
2022-05-26 22:15:24 -0700

    Update __init__.py

--- custom_components/linktap/__init__.py
+++ custom_components/linktap/__init__.py
@@ -15,8 +15,8 @@ from .device import LinkTapDeviceDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
-
+# PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
+PLATFORMS = []
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up flo from a config entry."""
