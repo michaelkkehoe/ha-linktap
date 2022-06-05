bfa7840f4a8c4020f81d599b3cf91efc3740eef9
Michael Kehoe <michael.k.kehoe@gmail.com>
2022-06-04 21:30:33 -0700

    Adding switch platform

--- custom_components/linktap/__init__.py
+++ custom_components/linktap/__init__.py
@@ -16,7 +16,7 @@ from .device import LinkTapDeviceDataUpdateCoordinator
 _LOGGER = logging.getLogger(__name__)
 
 # PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up flo from a config entry."""
